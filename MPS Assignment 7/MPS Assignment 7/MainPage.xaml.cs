b0ef99f31cef053eb44b63ec8d3fac87dffde93f
Caleb Jooste <cjooste@my365-uwc.ac.za>
2020-11-18 12:43:41 +0200

    Add project files.

---
+++ MPS Assignment 7/MPS Assignment 7/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MPS_Assignment_7
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CameraOption_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
